a944477def1b3f223bdcbe5f5a962f1647173357
fish155 <fishareverysmart@gmail.com>
2025-10-30 23:05:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1117,17 +1117,7 @@ class AdvancedMusicPlayer {
         div.textContent = text;
         return div.innerHTML;
     }
-    debounce(func, wait) {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func.apply(this, args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
+    
     
     attachSongElementListeners(songElement, song) {
         const favoriteBtn = songElement.querySelector(".favorite-btn");
